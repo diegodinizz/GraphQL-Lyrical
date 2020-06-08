580f962395022d78700c6b98457ffebf857ae1d4
Diego Diniz <diegodinizz@Diegos-MacBook-Pro.local>
2020-06-08 12:51:20 +0100

    changed the query file

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import getSong from '../queries/getSong'
 
 class SongDetail extends Component {
-  render() {
+  render () {
+    console.log(this.props)
     return (
       <div>
         <h3>Song Detail</h3>
@@ -10,4 +13,8 @@ class SongDetail extends Component {
   }
 }
 
-export default SongDetail
+export default graphql(getSong, {
+  options: props => {
+    return { variables: { id: props.params.id } }
+  }
+})(SongDetail)
